17154459db0a4c012d2c5a497cf49c08a355485c
ThreeT <7hr0771e@gmail.com>
2020-05-14 01:30:51 +0300

    Настройка событий в JS

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	const checkbox = document.querySelector('#main--checkbox');
+	const checkboxMonthly = document.querySelectorAll('.price_monthly');
+    const checkboxYear = document.querySelectorAll('.price_year');
+
+	if (checkbox.checked === false) {
+        for (let i = 0; i < checkboxMonthly.length; i++) {
+            checkboxMonthly[i].style.display = 'none';
+        }
+    } else if (checkbox === true) {
+        for (let i = 0; i < checkboxYear.length; i++) {
+            checkboxYear[i].style.display = 'none';
+        }
+    } else {
+	    console.log('Error - checkbox!!!');
+    }
 
 });
