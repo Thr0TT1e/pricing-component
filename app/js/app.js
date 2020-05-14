27a14b9e415e4602f3c71e5d75880eb8ad89857d
ThreeT <info@noble24.pro>
2020-05-14 18:43:02 +0300

    Реализация логики на JavaScript.

--- app/js/app.js
+++ app/js/app.js
@@ -4,16 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
 	const checkboxMonthly = document.querySelectorAll('.price_monthly');
     const checkboxYear = document.querySelectorAll('.price_year');
 
-	if (checkbox.checked === false) {
-        for (let i = 0; i < checkboxMonthly.length; i++) {
-            checkboxMonthly[i].style.display = 'none';
-        }
-    } else if (checkbox === true) {
-        for (let i = 0; i < checkboxYear.length; i++) {
-            checkboxYear[i].style.display = 'none';
-        }
-    } else {
-	    console.log('Error - checkbox!!!');
-    }
+    showPrice();
 
+	document.querySelector('#main--checkbox').onclick = showPrice;
+
+	function showPrice() {
+        if (checkbox.checked === false) {
+            for (let i = 0; i < checkboxYear.length, i < checkboxMonthly.length; i++) {
+                checkboxMonthly[i].style.display = 'none';
+                checkboxYear[i].style.display = 'block';
+            }
+        } else if (checkbox.checked === true) {
+            for (let i = 0; i < checkboxYear.length, i < checkboxMonthly.length; i++) {
+                checkboxYear[i].style.display = 'none';
+                checkboxMonthly[i].style.display = 'block';
+            }
+        } else {
+            console.log('Error - checkbox!!!');
+        }
+    }
 });
